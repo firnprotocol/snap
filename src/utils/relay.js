d9961504f894e9af8ce769ee089affd987460928
Firn Protocol <firnprotocol@protonmail.com>
2023-09-19 08:24:14 -0400

    simplify relay

--- src/utils/relay.js
+++ src/utils/relay.js
@@ -1,33 +1,22 @@
 export class Relay {
   constructor() {
-    this.relays = [
-      "https://www.firn.link",
-    ];
+    this.relay = "https://www.firn.link";
   }
 
   async fetch(endpoint, body) { // wraps regular fetch.
     // not doing any timeout for now.
-    let stored;
-    for (let i = 0; i < this.relays.length; i++) {
-      const init = {
-        method: "POST",
-        mode: "cors", // is this necessary?!? check.
-        headers: {
-          "Content-Type": "application/json"
-        },
-      };
-      if (body !== undefined) {
-        init.body = JSON.stringify(body);
+    const init = {
+      method: "POST",
+      mode: "cors", // is this necessary?!? check.
+      headers: {
+        "Content-Type": "application/json"
       }
-      try {
-        const response = await fetch(`${this.relays[i]}/${endpoint}`, init);
-        if (!response.ok) throw response; // does this ever happen?
-        return await response.json();
-      } catch (error) {
-        console.error(error);
-        stored = error;
-      }
+    };
+    if (body) {
+      init.body = JSON.stringify(body);
     }
-    throw stored;
+    const response = await fetch(`${this.relay}/${endpoint}`, init);
+    if (!response.ok) throw response; // does this ever happen?
+    return await response.json();
   }
 }
